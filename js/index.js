04ce13fd306fb8699d42f46de8353bf20d1d8df1
staccats <47672588+staccats@users.noreply.github.com>
2019-09-05 23:22:25 -0600

    DOM II - Started

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+
+//  NAV ---------------------
+const navFun = document.querySelectorAll('.nav-link');
+const navMouseOver = navFun.forEach(event => {
+    event.addEventListener('mouseover', (event) => {
+    event.target.style.fontSize = '2rem';
+    console.log('Nav Hover Activated');
+    })
+});
+
+const navMouseOverEnd = navFun.forEach(event => {
+    event.addEventListener('mouseout', (event) => {
+        event.target.style.fontSize = '1.6rem';
+    })
+})
+
+
+//  FUN BUS LOGO 
+
+// const logoFun = document.querySelector('.logo-heading');
+// logoFun.addEventListener('')
+
